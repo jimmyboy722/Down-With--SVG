28413acd9961ea09130678471558d3bcbb0b9cbc
jimmyboy722 <jeckman722@gmail.com>
2024-08-10 20:21:46 -0400

    Started shapes clases along with parent class

---
+++ Lib/shapes.js
@@ -0,0 +1,28 @@
+// PARENT CLASS
+// LEAVING THE COLOR AS AN EMPTY STRING TO BE POPULATED BY PROMPTED QUESTION
+class Shape {
+    constructor(){
+        this.color = '';
+    }
+}
+
+// USING THE EXTEND METHOD FOR EACH SPECIFIC SHAPE CLASS FOR ARGUMENTS FROM PARENT CLASS 'SHAPE'
+class Triangle {
+    render() {
+        return '<svg width=
+    }
+}
+// COLORS APPLIED VIA THE FILL PROPERTY
+class Circle {
+    render() {
+        return '<
+    }
+}
+
+class Square {
+    render() {
+        return '<svg width=
+    }
+}
+
+module.exports = 
