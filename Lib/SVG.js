9c3c2ccd9338716095cbddefa906c378737b00a0
jimmyboy722 <jeckman722@gmail.com>
2024-08-16 01:08:20 -0400

    finished terminal.js file for prompts to create SVG images

--- Lib/SVG.js
+++ Lib/SVG.js
@@ -1,24 +1,29 @@
-function SVG (){
+class SVG {
+  constructor() {
     // THESE PROPERTIES ARE SER TO EMPTY STRINGS AND WILL HOLD SVG ELEMENTS FOR TEXT AND SHAPES
-    this.textEl = '';
-    this.shapeEl = '';
+    this.textEl = "";
+    this.shapeEl = "";
+  }
 
-render() { 
+  render() {
     // RETURNS A THE STRING REPRESENTING THE SVG ELEMENT
     return `<svg version="1.1" width="200" height="200" xmlns="http://www.w3.org/2000/svg"> ${this.shapeElement} ${this.textElement} </svg>`;
-}
-// CREATING THE TEXT PROPERTY INCLUDING FONT SIZE, COLOR, AND LOCATION WITHIN THE SVG
-createText(message, color) {
+  }
+  // CREATING THE TEXT PROPERTY INCLUDING FONT SIZE, COLOR, AND LOCATION WITHIN THE SVG
+
+  createText(message, color) {
     if (message.length > 3) {
-     throw new Error('Maximum characters for message is 3.');
-    } else { 
-    this.textEl = `< text y="100" x="100"  font-size="30" text-anchor="middle" fill="${color}">${message}</text>`;
+      throw new Error("Maximum characters for message is 3.");
+    } else {
+      this.textEl = `< text y="100" x="100"  font-size="30" text-anchor="middle" fill="${color}">${message}</text>`;
+      console.log("It worked!");
     }
-}
-// CREATING THE SVG SHAPE
-createShape (shape) {
+  }
+  // CREATING THE SVG SHAPE
+  createShape(shape) {
     this.shapeEl = shape.render();
+  }
 }
-};
+
 // EXPORTING THIS CLASS SO IT'S AVAILABLE FOR IMPORT IN OTHER FILES
 module.exports = SVG;
--- Lib/terminal.js
+++ Lib/terminal.js
@@ -1,1 +1,76 @@
-const inquirer = require('inquirer');
+// FIRST SETTING UP IMPORTS
+const inquirer = require("inquirer");
+const writeFile = require("fs/promises");
+const SVG = require("./SVG");
+const { Triangle, Circle, Square } = require("./shapes");
+const { run } = require("jest");
+const { error } = require("console");
+
+function Terminal() {
+  return (
+    inquirer
+      // GROUP OF QUESTIONS REQUIRING INPUT FOR RENDERING OOF SVG'S
+      .prompt([
+        {
+          name: "shape",
+          type: "list",
+          message: "Pick an SVG shape",
+          choices: ["circle", "square", "triangle"],
+        },
+        {
+          name: "shapeColor",
+          type: "input",
+          message: "Select a color for your shape",
+        },
+        {
+          name: "text",
+          type: "input",
+          message:
+            "Please enter message to insert into SVG (no more than 3 characters.)",
+          confirmText: (text) =>
+            text.length <= 3 || "Text must not be more than 3 characters",
+        },
+        {
+          name: "textColor",
+          type: "input",
+          message: "Select a text color",
+        },
+      ])
+      //USING A THEN METHOD CHAINED TO A PROMISE TO LINK PRODUCING AND CONSUMING CODE / PLACE HOLDER FOR EXPECTED DATA
+      // USING AN ARROW FUNCTION AS AN ARGUMENT THAT WILL BE EXECUTED ONCE THE PROMISE IS RESOLVED
+      .then(({ shape, shapeColor, text, textColor }) => {
+        let shapeType;
+        //switch statement used to determine the type of shape to create
+        switch (shape) {
+          case "triangle":
+            shape = new Triangle();
+            break; // to ensure the switch statements are exited after completion
+
+          case "circle":
+            shape = new Circle();
+            break;
+
+          case "square":
+            shape = new Square();
+            break;
+        }
+        //pickColor method called to set color of the SVG
+        shape.pickColor(shapeColor);
+        // Creating and configuring SVG images
+        const svg = new SVG();
+        SVG.createText(text, textColor);
+        svg.createShape(shape);
+        // Writing the SVG to a file
+        return writeFile("images.svg", svg.render());
+      })
+      // Chaining another .then method to log successful image generation
+      .then(() => {
+        console.log("SVG generation successful!");
+      })
+      // Catch method for error reporting
+      .catch((error) => {
+        console.log(error);
+        console.log("Image could not be properly generated!");
+      })
+  );
+}
